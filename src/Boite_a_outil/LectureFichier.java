32c12481894440dc957b86c14279912da3655af3
JAYA971 <yaeljacob971@hotmail.fr>
2016-01-12 15:14:55 +0100

    boite a outils

---
+++ src/Boite_a_outil/LectureFichier.java
@@ -0,0 +1,62 @@
+package Boite_a_outil;
+
+import java.io.*;
+import java.util.*;
+
+import com.xmlparser.CsvFileHelper;
+import com.xmlparser.CsvFileRead;
+
+public class LectureFichier {
+
+	/**
+	 * Read a txt file
+	 * @param path Source file
+	 * @return String List
+	 */
+	public static List<String> readTXT(String path){
+		ArrayList<String> rows = new ArrayList<String>();
+		try{
+			InputStream ips=new FileInputStream(path); 
+			InputStreamReader ipsr=new InputStreamReader(ips);
+			BufferedReader br=new BufferedReader(ipsr);
+			String row;
+			
+			
+			while ((row=br.readLine())!=null){
+				//System.out.println(row);
+				rows.add(row);
+
+			}	
+			
+			br.close();
+		}catch (IOException ioe){
+			ioe.getStackTrace();
+		}
+		return rows;
+	}
+	
+	public static  List<String[]> readCSV(String path){
+		
+		List<String[]> lines = new ArrayList<String[]>();
+		File fileCsv = CsvFileHelper.getResource(path);
+		CsvFileRead StationCsv;
+		
+		StationCsv = new CsvFileRead(fileCsv);
+		lines = StationCsv.getData();
+		//vérification des données récupérédu fichier csv
+//		for (String[] colonne : lines)
+//		{
+//			for (int i = 0; i < colonne.length; i++) {
+//				System.out.println(colonne[i] + " ");
+//			}
+//			System.out.println("*******************");
+//			System.out.println(colonne[4]+", "+colonne[3]+", "+colonne[5]);
+//			
+//			//System.out.println(colonne[4]);
+//
+//		}
+				
+		
+		return lines;
+	}
+}
